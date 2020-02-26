f8b5554721a8f2173410dd91536fd3c3784832b5
Maya Mounzer <maeydelee14@yahoo.com>
2020-02-26 01:06:59 +0000

    Created section 9

--- e-commerce/client/src/core/Menu.js
+++ e-commerce/client/src/core/Menu.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link, withRouter } from 'react-router-dom'
+import {signout, isAuthenticated } from '../auth'
 
 const isActve = (history, path) => {
     if (history.location.pathname === path) {
@@ -9,17 +10,34 @@ const isActve = (history, path) => {
 
 function Menu({ history }) {
     return (
-        <div>
+        <Fragment>
             <ul className='nav nav-tabs bg-primary'>
-                <li className='nav-item'>
+
+            <li className='nav-item'>
+                    <Link className='nav-link' style={isActve(history, '/')} to='/'>Home </Link>
+                </li>
+                
+        {!isAuthenticated() && (
+            <div>
+            <li className='nav-item'>
                     <Link className='nav-link' style={isActve(history, '/signup')} to='/signup'>Sign Up</Link>
                 </li>
                 <li className='nav-item'>
                     <Link className='nav-link' style={isActve(history, '/signin')} to='/signin'>Sign In </Link>
                 </li>
+            </Fragment>
+        )}
+
+                {isAuthenticated() && (
+                    <Fragment>
                 <li className='nav-item'>
-                    <Link className='nav-link' style={isActve(history, '/')} to='/'>Home </Link>
+                    <span className='nav-link' style={{cursor: 'pointer', color: 'white'}} onClick={() => signout(() =>{
+                        history.push('/')
+                    })}>Sign out</span>
                 </li>
+                    </Fragment>
+                )}
+
             </ul>
         </div>
     )
