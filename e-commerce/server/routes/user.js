2e1932d9c8140aad423a1a572e0ab53d1379fee3
ChauPham90 <minhchaupham90@gmail.com>
2020-02-26 11:00:36 +0100

    season8 completed

--- e-commerce/server/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const { signup } = require('../controllers/user')
-router.get('/signup', (req, res) => {
-    res.send('hi')
-})
-router.post('/signup', signup)
-
-module.exports = router;
