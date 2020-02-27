0f804d4b3ca3740f30bea35656f28c0bdfa6fbd3
ChauPham90 <minhchaupham90@gmail.com>
2020-02-27 21:15:51 +0100

    revert to get server back

---
+++ e-commerce/server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+
+require('dotenv').config();
+// import mongoose
+const mongoose = require('mongoose');
+// import router
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
+
+//db connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+// route middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+app.use('/api', authRouter);
+app.use('/api', userRouter);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`app listen to ${PORT} `));
